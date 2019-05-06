e437ea4ab6b12ffbe08c3680d417b62e7e532b74
Stanislav Starkov <s.starkov@corp.mail.ru>
2019-05-06 17:16:41 +0300

    fix

--- terraform/EKS/main.tf
+++ terraform/EKS/main.tf
@@ -1,19 +1,3 @@
-### EKS Security Group
-resource "aws_security_group" "war-eks-cluster" {
-  name        = "${var.cluster-name}"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = "${data.aws_vpc.game_vpc.id}"
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = "${merge(var.tags, map("Name", var.cluster-name))}"
-}
-
 resource "aws_eks_cluster" "war-dev-eks" {
   #  enabled_cluster_log_types = ["api", "audit"]
   name     = "${var.cluster-name}"
@@ -27,13 +11,3 @@ resource "aws_eks_cluster" "war-dev-eks" {
     security_group_ids = ["${aws_security_group.war-eks-cluster.id}"]
   }
 }
-
-output "endpoint" {
-  value = "${aws_eks_cluster.war-dev-eks.endpoint}"
-}
-
-#
-#output "kubeconfig-certificate-authority-data" {
-#  value = "${aws_eks_cluster.example.certificate_authority.0.data}"
-#}
-
